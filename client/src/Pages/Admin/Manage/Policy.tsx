f3e8adf0d7f5dfa8b615e8e64e69a9f46d7a3d94
Harold Graphs <haroldcastillo272@gmail.com>
2023-11-14 08:54:45 +0800

    feat(polict):create modal for uploading

--- client/src/Pages/Admin/Manage/Policy.tsx
+++ client/src/Pages/Admin/Manage/Policy.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
-import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button'
-import ModeEditIcon from '@mui/icons-material/ModeEdit';
-
+import { Modal,Grid,TextField } from '@mui/material';
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+    borderRadius:"8px",
+    maxHeight:"90vh",
+    overflowY:"scroll"
+};
 function Policy() {
+    const [open, setOpen] = useState("");
     return <>
         <Typography variant="h4" fontWeight={600} color="primary">Manage Policy </Typography>
         <Typography variant="h6" fontWeight={400} color="initial" sx={{marginBottom:"2em"}}>Update your policy</Typography>
@@ -14,7 +26,7 @@ function Policy() {
                 <Box sx={{flexGrow:"1",display:"flex"}}>
                     
                 </Box>
-                <Button variant="contained" color="primary" onClick={()=>{}}>
+                <Button variant="contained" color="primary" onClick={()=>{setOpen("editPDF")}}>
                     Upload PDF
                 </Button>
             </Box>
@@ -23,6 +35,49 @@ function Policy() {
                 <PictureAsPdfIcon sx={{fill:"#626262",fontSize:"100px"}}/>
             </Box>
         </Box>
+        <Modal
+            keepMounted
+            open={!(open==="")}
+            onClose={()=>{setOpen("")}}
+            aria-labelledby="keep-mounted-modal-title"
+            aria-describedby="keep-mounted-modal-description"
+        >
+            <Box sx={style}>
+                {open === "editPDF"?<>
+                    <Typography id="keep-mounted-modal-title" variant="h6" fontWeight={700} color={"primary"} component="h2">
+                        Upload PDF
+                    </Typography>
+                    <Typography id="keep-mounted-modal-description" sx={{marginBottom:"15px"}}>
+                        Set policy for the guests
+                    </Typography>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} >
+                            <TextField
+                                type='file'
+                                id="policy"
+                                fullWidth
+                            />
+                        </Grid>
+                        
+                        <Grid item xs={12} padding={"1em 0"}>
+                            
+                        </Grid>
+                        <Grid item xs={5}>
+                            <Button variant="text" fullWidth onClick={()=>{setOpen("")}}>
+                                back
+                            </Button>
+                        </Grid>
+                        <Grid item xs={7}>
+                            <Button variant="contained" color='primary' fullWidth onClick={()=>setOpen("")}>
+                                Confirm
+                            </Button>
+                        </Grid>
+                    </Grid>
+                
+                </>:""}
+
+            </Box>
+        </Modal>
     </>
 }
 
