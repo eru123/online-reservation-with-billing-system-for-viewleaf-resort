ad8f19286bb94b8c8e241a28d065a0373018fdd4
Harold Graphs <haroldcastillo272@gmail.com>
2023-11-14 17:11:37 +0800

    feat(error): change context

--- client/src/Pages/Error.tsx
+++ client/src/Pages/Error.tsx
@@ -6,8 +6,8 @@ function Error() {
   return (
     <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",height:"100vh"}}>
         <img src={Image}  width={"30%"} alt="" />
-        <Typography variant="h4" color="primary" fontWeight={500} mt={"25px"}>404 Error</Typography>
-        <Typography variant="h6" color="primary">Sorry, Page Not Found</Typography>
+        <Typography variant="h4" color="primary" fontWeight={500} mt={"25px"}>Error Page</Typography>
+        <Typography variant="h6" color="primary">Sorry, theres a problem</Typography>
     </div>
   )
 }
