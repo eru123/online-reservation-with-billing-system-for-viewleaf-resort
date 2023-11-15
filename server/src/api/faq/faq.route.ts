d59c300ff5d2429e7429b344e47991426d94d986
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-16 01:28:40 +0800

    feat(faq): create faq route

---
+++ server/src/api/faq/faq.route.ts
@@ -0,0 +1,19 @@
+// routes.ts
+import express from 'express';
+import {
+  createFAQs,
+  // getFAQs,
+  // editFAQ,
+  // deleteFAQ,
+} from './faq.controller';
+import { Router } from 'express';
+import asynchronousHandler from '../../middlewares/asynchronousHandler';
+
+const router = express.Router();
+
+router.post('/', asynchronousHandler(createFAQs));
+// router.get('/faqs', asynchronousHandler(getFAQs));
+// router.patch('/faqs', asynchronousHandler(editFAQ));
+// router.delete('/faqs', asynchronousHandler(deleteFAQ));
+
+export default router;
