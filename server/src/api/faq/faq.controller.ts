dd81cbde305f8401682f847a8cc10b85ddf81b22
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-16 01:28:15 +0800

    feat(faq): create faq controller

---
+++ server/src/api/faq/faq.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import FAQModel from './faq.model';
+
+import { CreateFAQInput, FAQDocument } from './faq.types';
+
+export const createFAQs = async (req: Request<any, any, CreateFAQInput>, res: Response) => {
+  try {
+    console.log(req.body);
+    const { question, answer } = req.body;
+
+    const faq = await FAQModel.create({ 
+      question, 
+      answer 
+    });
+    console.log(faq);
+
+    res.json(faq);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
