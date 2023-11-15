12970c152b995faa92bfbb1510a775257771721e
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-16 02:13:01 +0800

    feat(content): create content types

---
+++ server/src/api/content/content.types.ts
@@ -0,0 +1,9 @@
+export interface ContentDocument extends Document {
+  about: string;
+  phone: number;
+  email: string,
+  address: string,
+  policy: string,
+  payment: string,
+  promo: string,
+}
