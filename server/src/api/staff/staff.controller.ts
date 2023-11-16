b278970a7cbb3a8861fed48eab0a427190aa2384
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-11-16 21:00:36 +0800

    feat(staff): add staff registration

--- server/src/api/staff/staff.controller.ts
+++ server/src/api/staff/staff.controller.ts
@@ -2,9 +2,29 @@ import { BodyRequest, RequestHandler } from 'express';
 import { CheckData } from '../../utilities/checkData';
 import { compareSync } from 'bcrypt';
 import { cookieOptions, signAccess, signRefresh } from '../../utilities/cookies';
+import { CreateStaff, Role } from './staff.types';
 import { Login } from '../auth/auth.types';
+import { password } from '../../utilities/ids';
 import { Unauthorized, UnprocessableEntity } from '../../utilities/errors';
 import StaffModel from './staff.model';
+
+export const createStaff: RequestHandler = async (req: BodyRequest<CreateStaff>, res) => {
+    const { username, email } = req.body;
+    
+    const checker = new CheckData();
+    checker.checkType(username, 'string', 'username');
+    checker.checkType(email, 'string', 'email');
+    if (checker.size() > 0) throw new UnprocessableEntity(checker.errors);
+
+    const credentials = { email, password: password() }
+    await StaffModel.create({
+        username,
+        credentials,
+        role: Role.STAFF
+    });
+
+    res.status(201).json(credentials);
+}
 
 export const login: RequestHandler = async (req: BodyRequest<Login>, res) => {
     const { email, password } = req.body;
