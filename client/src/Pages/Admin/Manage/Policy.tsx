3ffb850d7f5007a164a08ead4333ab32dbe442ef
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-17 00:08:17 +0800

    feat(content): view & update policy

--- client/src/Pages/Admin/Manage/Policy.tsx
+++ client/src/Pages/Admin/Manage/Policy.tsx
@@ -1,9 +1,13 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import Button from '@mui/material/Button'
 import { Modal,Grid,TextField } from '@mui/material';
+
+import useContent from '../../../Hooks/useContent';
+import useFirebase from '../../../Hooks/useFirebase';
+
 const style = {
     position: 'absolute' as 'absolute',
     top: '50%',
@@ -17,8 +21,20 @@ const style = {
     maxHeight:"90vh",
     overflowY:"scroll"
 };
+
 function Policy() {
     const [open, setOpen] = useState("");
+    const {data:content, loading:contentLoading, error:contentError, getContent, updateContent} = useContent();
+    const {
+      downloadURL, 
+      uploading, 
+      uploadFile
+    } = useFirebase();
+
+    useEffect(()=>{
+      getContent();
+    },[])
+
     return <>
         <Typography variant="h4" fontWeight={600} color="primary">Manage Policy </Typography>
         <Typography variant="h6" fontWeight={400} color="initial" sx={{marginBottom:"2em"}}>Update your policy</Typography>
@@ -33,6 +49,13 @@ function Policy() {
         <Box>
             <Box display="flex" justifyContent={"center"} alignItems={"center"}  minHeight={"800px"} sx={{background:"#B8B8B8",borderRadius:"12px"}}>   
                 <PictureAsPdfIcon sx={{fill:"#626262",fontSize:"100px"}}/>
+                <iframe
+                  title="PDF Viewer" 
+                  src={content?.policy} 
+                  width="100%"
+                  height="500px" // You can adjust the height based on your preference
+                  frameBorder="0"
+                />
             </Box>
         </Box>
         <Modal
@@ -57,6 +80,12 @@ function Policy() {
                                 id="policy"
                                 fullWidth
                             />
+                            <input
+                              type="file"
+                              accept=".pdf"
+                              onChange={(e:any)=>{uploadFile(e.target.files[0], 'orbs')}}
+                              id="file-upload-input"
+                            />
                         </Grid>
                         
                         <Grid item xs={12} padding={"1em 0"}>
@@ -68,7 +97,10 @@ function Policy() {
                             </Button>
                         </Grid>
                         <Grid item xs={7}>
-                            <Button variant="contained" color='primary' fullWidth onClick={()=>setOpen("")}>
+                            <Button variant="contained" color='primary' fullWidth onClick={()=>{
+                              setOpen("");
+                              updateContent({policy: downloadURL});
+                            }}>
                                 Confirm
                             </Button>
                         </Grid>
