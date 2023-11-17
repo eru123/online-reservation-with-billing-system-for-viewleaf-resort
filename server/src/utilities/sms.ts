e8a121abc0cca8188457fe1fc5f5e48064358ebd
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-17 18:25:23 +0800

    refactor: export send sms

--- server/src/utilities/sms.ts
+++ server/src/utilities/sms.ts
@@ -5,13 +5,13 @@ const authToken = TWILLIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 // Example Request Body
-const smsDetails = {
-  body: 'message',
-  from: '+19382532343',
-  to: '+639082657587',
-};
+// const smsDetails = {
+//   body: 'message',
+//   from: '+19382532343',
+//   to: '+639082657587',
+// };
 
-const sendSMS = (smsDetails: { body: any; from: any; to: any; }) => {
+export const sendSMS = (smsDetails: { body: any; from: any; to: any; }) => {
   const { body, from, to } = smsDetails;
 
   return client.messages
@@ -31,7 +31,3 @@ const sendSMS = (smsDetails: { body: any; from: any; to: any; }) => {
       throw error;
     });
 };
-
-// sendSMS(smsDetails);
-
-module.exports = { sendSMS };
