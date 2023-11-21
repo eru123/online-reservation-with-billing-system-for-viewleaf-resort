edd31d598aca9ac983111fd0445cc3c776790b9d
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-22 00:18:17 +0800

    feat(email): create email controller

---
+++ server/src/api/email/email.controller.ts
@@ -0,0 +1,39 @@
+import { BodyRequest, RequestHandler } from 'express';
+import { Email } from './email.types';
+import envs from '../../utilities/envs';
+
+const nodemailer = require('nodemailer');
+const { NODEMAILER_EMAIL, NODEMAILER_PASSWORD } = envs;
+
+export const sendEmail: RequestHandler = async (req: BodyRequest<Email>, res) => {
+  const { to, subject, content } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'outlook',
+    auth: {
+      user: NODEMAILER_EMAIL,
+      pass: NODEMAILER_PASSWORD,
+    },
+  });
+
+  // Email options
+  const mailOptions = {
+    from: NODEMAILER_EMAIL, // Sender address
+    to: to, // Recipient address
+    subject: subject, // Subject line
+    html: content // HTML body
+  };
+
+  try {
+    // Send email
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ', info.messageId);
+    res.json(info);
+  } catch (error: any) {
+    console.error('Error sending email: ', error.message);
+    // General error response
+    res.status(500).json({ error: 'Error sending email', message: error.message });
+  }
+
+};
+
