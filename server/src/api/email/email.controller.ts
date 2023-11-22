b8f0d33cdcf97b17146b65dbc29cb569bc167233
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-22 22:10:07 +0800

    refactor: optimize send email controller

--- server/src/api/email/email.controller.ts
+++ server/src/api/email/email.controller.ts
@@ -16,22 +16,19 @@ export const sendEmail: RequestHandler = async (req: BodyRequest<Email>, res) =>
     },
   });
 
-  // Email options
-  const mailOptions = {
-    from: NODEMAILER_EMAIL, // Sender address
-    to: to, // Recipient address
-    subject: subject, // Subject line
-    html: content // HTML body
-  };
-
   try {
     // Send email
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent: ', info.messageId);
+    const info = await transporter.sendMail({
+      from: NODEMAILER_EMAIL, // Sender address
+      to: to, // Recipient address
+      subject: subject, // Subject line
+      html: content // HTML body
+    });
+
     res.json(info);
+
   } catch (error: any) {
-    console.error('Error sending email: ', error.message);
-    // General error response
+
     res.status(500).json({ error: 'Error sending email', message: error.message });
   }
 
