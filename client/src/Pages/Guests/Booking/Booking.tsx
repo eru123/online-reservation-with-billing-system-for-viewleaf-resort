6109f33260ecfd3cf278e489755d4a53101836ce
Harold Graphs <haroldcastillo272@gmail.com>
2023-12-01 21:38:45 +0800

    fix: verify modal

--- client/src/Pages/Guests/Booking/Booking.tsx
+++ client/src/Pages/Guests/Booking/Booking.tsx
@@ -2,20 +2,21 @@ import React,{useState, useEffect} from 'react'
 import Container from '@mui/material/Container'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
 
 // Tabs
 import Accommodation from './Accommodation'
 import GuestDetails from './GuestDetails'
 import BookingStatement from './BookingStatement'
-import Payment from './Payment'
 import { useParams } from 'react-router-dom'
+import TextField from '@mui/material/TextField'
 
-import useAccommodation from '../../../Hooks/useAccommodation'
 import useEmail from '../../../Hooks/useEmail'
 import useReservation from '../../../Hooks/useReservation'
+import Button from '@mui/material/Button'
+import Modal from '@mui/material/Modal';
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
 
 interface ReservationForm {
   name?: string;
@@ -43,8 +44,20 @@ interface CustomerData {
   email?: string;
   phone?: string;
 }
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 500,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+  borderRadius:"8px"
+};
 
 function Booking() {
+  const [open, setOpen] = React.useState("");
   const {sendEmail} = useEmail();
   const {createReservation} = useReservation();
   const [active,setActive] =  useState(1);
@@ -282,7 +295,11 @@ function Booking() {
                       back
                   </Button>
                   {active===3?
-                    <Button variant="contained" color="primary" onClick={()=> {sendVerification()}}>
+                    <Button variant="contained" color="primary" 
+                    onClick={()=> {
+                      sendVerification()
+                      setOpen("verify")
+                    }}>
                         Finish
                     </Button>
                   :
@@ -293,6 +310,44 @@ function Booking() {
               </div>
           </Container> 
       </Paper>
+      <Modal
+          keepMounted
+          open={!(open==="")}
+          onClose={()=>{setOpen("")}}
+          aria-labelledby="keep-mounted-modal-title"
+          aria-describedby="keep-mounted-modal-description"
+        >
+            <Box sx={style}>
+                {open === "verify"?<>
+                    <Typography id="keep-mounted-modal-title" variant="h6" fontWeight={600} color={"primary"} component="h2">
+                        Email verification
+                    </Typography>
+                    <Typography id="keep-mounted-modal-description" sx={{marginBottom:"15px"}}>
+                        We already email you a 6 digit code, Please check your email and enter the code to complete the verification
+                    </Typography>
+                    <Grid container spacing={2} sx={{marginTop:"35px"}}>
+                      <Grid item  xs={8}>
+                        <TextField
+                          id="OTP"
+                          label="OTP"
+                          required
+                          fullWidth
+                        />
+                      </Grid>
+                      <Grid item xs={4}>
+                        <Button variant="contained" color="primary" fullWidth sx={{height:"100%",background:"#414141"}} onClick={()=>setOpen("verify")}>
+                          Verify
+                        </Button>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Button variant="contained" color="primary" fullWidth sx={{marginTop:"25px",background:"white",color:"black"}} onClick={()=>setOpen("")}>
+                          cancel
+                        </Button>
+                      </Grid>
+                    </Grid>
+                </>:""}
+            </Box>
+        </Modal>
     </Container>
   )
 }
