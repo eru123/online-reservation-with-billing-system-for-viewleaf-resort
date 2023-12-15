af34de2a86275fce2f4ade79eaadff54d4c1c19e
Harold Graphs <haroldcastillo272@gmail.com>
2023-12-16 06:32:01 +0800

    fix chatbot bring back changes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,61 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './Hooks/useAuth';
 import './Styles/main.scss';
 
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-
-// root.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <AuthProvider>
-//         <Routes>
-//           <Route path="/*" element={<App/>}/>
-//         </Routes>
-//       </AuthProvider>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
-const body = ReactDOM.createRoot(document.body);
-
-body.render(
-  <>
-    <React.StrictMode>
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route path="/*" element={<App/>}/>
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </React.StrictMode>
-    <div id="fb-root"></div>
-    <div id="fb-customer-chat" className="fb-customerchat"></div>
-  </>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
 
-var chatbot = document.getElementById('fb-customer-chat');
-chatbot?.setAttribute("page_id", "191293380731152");
-chatbot?.setAttribute("attribution", "biz_inbox");
-
-(window as any).fbAsyncInit = function () {
-  (window as any).FB?.init({
-    xfbml: true,
-    version: 'v18.0',
-  });
-};
-
-(function (d, s, id) {
-  const js: HTMLScriptElement = d.createElement(s) as HTMLScriptElement; // Type assertion
-  const fjs = d.getElementsByTagName(s)[0];
-  if (!fjs || !js) return;
-  js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-  fjs.parentNode?.insertBefore(js, fjs);
-})(document, 'script', 'facebook-jssdk');
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App/>}/>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
