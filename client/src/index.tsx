3fdb386e900e9324799af4fd21b7c6c9f72438a3
Chrystarin <chrystarin@gmail.com>
2023-12-15 22:14:27 +0800

    test: chatbot

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,52 +6,61 @@ import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './Hooks/useAuth';
 import './Styles/main.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/*" element={<App/>}/>
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
-// const chatRoot = ReactDOM.createRoot(document.body);
-
-// chatRoot.render(
-//   <>
-//     <div id="fb-root"></div>
-//     <div className="fb-customerchat"></div>
-//   </>
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
 // );
 
-// if (window.location.pathname === '/') {
-//   var chatbot = document.getElementById('fb-customer-chat');
-//   chatbot?.setAttribute("page_id", "191293380731152");
-//   chatbot?.setAttribute("attribution", "biz_inbox");
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <AuthProvider>
+//         <Routes>
+//           <Route path="/*" element={<App/>}/>
+//         </Routes>
+//       </AuthProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 
-//   (window as any).fbAsyncInit = function () {
-//     (window as any).FB?.init({
-//       xfbml: true,
-//       version: 'v18.0',
-//     });
-//   };
+const body = ReactDOM.createRoot(document.body);
 
-//   (function (d, s, id) {
-//     const js: HTMLScriptElement = d.createElement(s) as HTMLScriptElement; // Type assertion
-//     const fjs = d.getElementsByTagName(s)[0];
-//     if (!fjs || !js) return;
-//     js.id = id;
-//     js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-//     fjs.parentNode?.insertBefore(js, fjs);
-//   })(document, 'script', 'facebook-jssdk');
-// }
+body.render(
+  <>
+    <React.StrictMode>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/*" element={<App/>}/>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+    <div id="fb-root"></div>
+    <div className="fb-customerchat"></div>
+  </>
+);
+
+if (window.location.pathname === '/') {
+  var chatbot = document.getElementById('fb-customer-chat');
+  chatbot?.setAttribute("page_id", "191293380731152");
+  chatbot?.setAttribute("attribution", "biz_inbox");
+
+  (window as any).fbAsyncInit = function () {
+    (window as any).FB?.init({
+      xfbml: true,
+      version: 'v18.0',
+    });
+  };
+
+  (function (d, s, id) {
+    const js: HTMLScriptElement = d.createElement(s) as HTMLScriptElement; // Type assertion
+    const fjs = d.getElementsByTagName(s)[0];
+    if (!fjs || !js) return;
+    js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode?.insertBefore(js, fjs);
+  })(document, 'script', 'facebook-jssdk');
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
