c0364793c34e5fe37f1068d00577e64c9d00204c
Chrystarin <chrystarin@gmail.com>
2023-12-15 22:07:04 +0800

    fix: chatbot issue

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
-import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './Hooks/useAuth';
-import './Styles/main.scss'
+import './Styles/main.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -20,6 +21,37 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+const chatRoot = ReactDOM.createRoot(document.body);
+
+chatRoot.render(
+  <>
+    <div id="fb-root"></div>
+    <div className="fb-customerchat"></div>
+  </>
+);
+
+if (window.location.pathname === '/') {
+  var chatbot = document.getElementById('fb-customer-chat');
+  chatbot?.setAttribute("page_id", "191293380731152");
+  chatbot?.setAttribute("attribution", "biz_inbox");
+
+  (window as any).fbAsyncInit = function () {
+    (window as any).FB?.init({
+      xfbml: true,
+      version: 'v18.0',
+    });
+  };
+
+  (function (d, s, id) {
+    const js: HTMLScriptElement = d.createElement(s) as HTMLScriptElement; // Type assertion
+    const fjs = d.getElementsByTagName(s)[0];
+    if (!fjs || !js) return;
+    js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode?.insertBefore(js, fjs);
+  })(document, 'script', 'facebook-jssdk');
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
