6f93c7eaa027fe8f82aae56c5c26c4d71a770856
eru123 <yeoligoakino@gmail.com>
2023-12-20 13:30:22 +0800

    Fix formatting and syntax issues in useAuth.tsx

--- client/src/Hooks/useAuth.tsx
+++ client/src/Hooks/useAuth.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import {useNavigate, Outlet, Navigate} from 'react-router-dom';
+import { useNavigate, Outlet, Navigate } from 'react-router-dom';
 import axios from './useAxios';
 
 interface AuthContextState {
@@ -23,11 +23,11 @@ interface LoginData {
 
 export const AuthContext = createContext<AuthContextState>({
   user: null,
-  login: () => {},
-  logout: () => {},
-  register: () => {},
+  login: () => { },
+  logout: () => { },
+  register: () => { },
   isAuth: () => false,
-  User: () => {}
+  User: () => { }
 });
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
@@ -35,12 +35,12 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const [user, setUser] = useState<any>(null);
 
   const login = async (data: LoginData) => {
-    const { email, password} = data;
-      try{
-        await axios
-        .post(`/staffs/login`,{
-          "email" : email,
-          "password" : password
+    const { email, password } = data;
+    try {
+      await axios
+        .post(`/staffs/login`, {
+          "email": email,
+          "password": password
         })
         .then((response: any) => {
           console.log(response.data)
@@ -48,30 +48,30 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
           localStorage.setItem('user', JSON.stringify(response.data))
           navigate("/admin")
         });
-      }
-      catch (error: any){
-        console.log(error);
-        alert(error.message);
+    }
+    catch (error: any) {
+      console.log(error);
+      // alert(error.message);
     }
   };
 
   const register = async (data: RegisterData) => {
-    const { username, email} = data;
+    const { username, email } = data;
 
-    try{
+    try {
       await axios
-      .post(`/staffs/register`,{
-        username: username,
-        email: email,
-      })
-      .then((response: any) => {
-        console.log(response)
-        setUser(response.data.user);
-      });
+        .post(`/staffs/register`, {
+          username: username,
+          email: email,
+        })
+        .then((response: any) => {
+          console.log(response)
+          setUser(response.data.user);
+        });
     }
-    catch (error: any){
+    catch (error: any) {
       console.log(error);
-      alert(error.message);
+      // alert(error.message);
     }
   };
 
@@ -81,7 +81,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     navigate("/login");
   };
 
-  const isAuth = (id:any) => {
+  const isAuth = (id: any) => {
     if (!user) {
       // User is not logged in, so they are not authorized
       return false;
@@ -89,7 +89,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
     // User is logged in and authorized
     return true;
-	};
+  };
 
   const User = () => {
     return localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') || '') : '';
@@ -111,7 +111,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 };
 
 export const useAuth = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
 
 interface ProtectedRouteProps {
@@ -127,14 +127,14 @@ export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ allowedRoles }) 
     // Checks if user exists
     user
       // Checks if allowed roles are defined
-      ? allowedRoles 
-          // Checks if user type is part of allowed roles
-          ? allowedRoles?.includes(user.role)
-              ? <Outlet/>
-              // Redirects to forbidden if user is not allowed
-              : <Navigate to="/"/>
-          : <Outlet/>   
+      ? allowedRoles
+        // Checks if user type is part of allowed roles
+        ? allowedRoles?.includes(user.role)
+          ? <Outlet />
+          // Redirects to forbidden if user is not allowed
+          : <Navigate to="/" />
+        : <Outlet />
       // Redirects to login if not logged in
-      : <Navigate to="/"/>
+      : <Navigate to="/" />
   );
 };
