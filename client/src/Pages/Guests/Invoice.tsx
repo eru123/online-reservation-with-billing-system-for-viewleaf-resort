15a1e67f1a77418d3e5b3ed8b50508f60032b485
eru123 <yeoligoakino@gmail.com>
2023-12-20 13:31:34 +0800

    Change invoice page link

--- client/src/Pages/Guests/Invoice.tsx
+++ client/src/Pages/Guests/Invoice.tsx
@@ -143,7 +143,7 @@ function Invoice({}:Props) {
           {status==="pending"?
             <Box sx={{position:"relative"}}>
               <Alert severity="warning" sx={{margin:"2em 0",padding:" 1em 9em 1em 1em"}}>Please make the payment to finalize your reservation, Thank you!</Alert>
-              <Chip label="Pay now" variant="outlined" onClick={()=>{}} component={Link} to={`/payment/${data?.[0]?.reservationId}`} color="primary" sx={{position:"absolute",top:"50%",right:"10px",transform:"translateY(-50%)"}}/>   
+              <Chip label="Pay now" variant="outlined" onClick={()=>{}} component={Link} to={data?.[0]?.paymongo?.url} color="primary" sx={{position:"absolute",top:"50%",right:"10px",transform:"translateY(-50%)"}} target='_blank'/>
             </Box>
           :""}
           {status==="paid"?
