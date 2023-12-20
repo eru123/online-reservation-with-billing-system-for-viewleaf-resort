3488858d63d9e4e9e00be978a33460608388f28f
eru123 <yeoligoakino@gmail.com>
2023-12-20 08:19:44 +0800

    Add semaphore utility for sending SMS messages

---
+++ server/src/utilities/semaphore.ts
@@ -0,0 +1,20 @@
+const {
+    SEMAPHORE_KEY,
+    SEMPAHORE_SENDER,
+} = process.env;
+
+import axios from 'axios';
+
+export const send: (number: string, message: string) => Promise<unknown> = async (number, message) => {
+    const url = 'https://api.semaphore.co/api/v4/messages';
+    const data: Record<string, string> = {
+        apikey: String(SEMAPHORE_KEY),
+        number,
+        message,
+        sendername: String(SEMPAHORE_SENDER),
+    };
+
+    return axios.post(url, data, {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    }).then(e => e.data);
+}
