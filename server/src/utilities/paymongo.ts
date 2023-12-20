b7a00e7b994bd54788e8411ef4890651a7408775
eru123 <yeoligoakino@gmail.com>
2023-12-20 08:19:39 +0800

    Add Paymongo utility functions

---
+++ server/src/utilities/paymongo.ts
@@ -0,0 +1,55 @@
+const {
+    PAYMONGO_SECRET,
+} = process.env;
+
+import axios from 'axios';
+
+export type PaymentData = {
+    amount: number;
+    description: string;
+    remarks?: string;
+};
+
+export const create = async (attr: PaymentData): Promise<unknown> => {
+    const url = 'https://api.paymongo.com/v1/links';
+    attr.amount = (Math.round((Number(attr.amount) + Number.EPSILON) * 100) / 100) * 100;
+    const payload = {
+        data: {
+            attributes: attr,
+        },
+    };
+
+    return axios.post(url, payload, {
+        headers: {
+            'Authorization': `Basic ${Buffer.from(`${PAYMONGO_SECRET}:`).toString('base64')}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+    })
+        .then(e => e.data)
+        .then(e => ({
+            id: e.data.id,
+            url: e.data.attributes.checkout_url,
+            ref: e.data.attributes.reference_number,
+            status: e.data.attributes.status,
+        }))
+};
+
+export const retrieve = async (id: string): Promise<unknown> => {
+    const url = `https://api.paymongo.com/v1/links/${id}`;
+
+    return axios.get(url, {
+        headers: {
+            'Authorization': `Basic ${Buffer.from(`${PAYMONGO_SECRET}:`).toString('base64')}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+    })
+        .then(e => e.data)
+        .then(e => ({
+            id: e.data.id,
+            url: e.data.attributes.checkout_url,
+            ref: e.data.attributes.reference_number,
+            status: e.data.attributes.status,
+        }))
+}
